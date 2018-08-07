591f50b7d508eb3cf32b90333a01f3d7eaacfb52
Armin <40608213+ArminNy@users.noreply.github.com>
2018-08-08 01:09:11 +0430

    up

---
+++ Config.lua
@@ -0,0 +1,17 @@
+Config = {
+SUDO_ID = {377450049,491608373,492979642}, --آیدی سودو های ربات
+Full_Sudo = {377450049}, -- آیدی سودو اصلی
+TD_ID = 492979642, -- آیدی ربات
+Sudoid = 377450049,-- آیدی سودو اصلی
+BotHelper = 491608373, -- آیدی هلپر
+UserBotHelper = '@HelperCompanybot', -- یوزرنیم هلپر
+LinkSuppoRt = 'https://t.me/joinchat/Fn9uQVENnVmkWhbSWuSIWA', -- لینک گروه ساپورت
+UserSudo = '@MahDiRoO', -- یوزرنیم سازنده
+PvUserSudo = '@MahDiRoO_Bot', -- پیام رسان سازنده
+Channel = '@MaTaDoRTeaM', -- یوزرنیم کانال 
+ChannelInline = 'MaTaDoRTeaM', --یوزنیم کانال بدون @
+RedisIndex = 1, -- شمار رديس را در اينجا قرار دهيد (اگر نمیدونید چی هست تغییری ایجاد نکنید) --> Redis:Select ...
+token = '491608373:AAGvo9nenU2gdOlHUjRoWacenNwJTVdj-58', -- توکن ربات api
+}
+
+return Config
